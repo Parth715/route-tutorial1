6752ab2d7263c984395e63f38c9d65fc67f53aec
Parth Patel <parth2002715@gmail.com>
2021-11-22 16:22:07 -0500

    added a search filter input

--- src/app/userapps/home/home.component.ts
+++ src/app/userapps/home/home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  objs:any[] = [
+    { id: 9, title: "ABC", count: 40 },
+    { id: 5, title: "GHI", count: 24 },
+    { id: 17, title: "aaa", count: 75 },
+    { id: 2, title: "zzz", count: 1},
+    { id: 3, title: "DEF", count: 16 },
+    { id: 7, title: "JKL", count: 32 },
+    { id: 1, title: "ZYX", count: 8 }
+  ];
+  searchtext: string = "";
+  aBool: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
